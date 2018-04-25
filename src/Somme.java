55187ae0bc8f3ad555bdeab2ec2c49a4de17d288
BurningPigM <mickael.carpene@gmail.com>
2018-04-25 11:29:28 +0200

    DEV1

---
+++ src/Somme.java
@@ -0,0 +1,14 @@
+
+public class Somme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(somme(1,5));
+	}
+	
+	
+	public static int somme(int a, int b) {
+		return a+b;
+	}
+
+}
